768900cb4175194fe2f8de92ab790ce7bfe70614
Chris Cowan <chriscowan@plus3network.com>
2013-03-22 18:43:16 -0700

    Inital commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var puppeteer = module.exports = function () {
+  console.log("This is going to do something magical... just wait an see!"); 
+};
+
+if (require.module === module) {
+  puppeteer();
+}
